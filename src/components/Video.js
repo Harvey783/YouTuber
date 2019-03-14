8c36399ecd83018ea28808ab72db5f15a3b9b97f
Chris <chharvey@icloud.com>
2019-03-14 19:29:21 -0400

    styling

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './Video.css';
 
 const Video = ({ video }) => {
   return (
-    <div className="collection-item-avatar">
+    <div className="video-collection collection-item avatar">
       <img src={video.snippet.thumbnails.medium.url} alt="" />
-      <span className="title">{video.snippet.title}</span>
+      <span className="video-title title">{video.snippet.title}</span>
     </div>
   );
 };
 
 export default Video;
+
+/* <div className="video-collection collection-item-avatar">
+<img src={video.snippet.thumbnails.medium.url} alt="" />
+<span className="title">{video.snippet.title}</span>
+</div> */
+
+/* <li className="video-avatar collection-item avatar">
+<img src={video.snippet.thumbnails.medium.url} alt="" />
+<span className="video-list-title title">{video.snippet.title}</span>
+</li> */
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,13 +5,13 @@ const VideoList = ({ videos }) => {
   const renderVideoList = videos.map(video => {
     return <Video key={video.id.videoId} video={video} />;
   });
-  return (
-    <div>
-      <ul className="collection">{renderVideoList}</ul>
-    </div>
-  );
+  return <div className="collection">{renderVideoList}</div>;
 };
 
 export default VideoList;
 
 /* <div className="collection">{renderVideoList}</div>; */
+
+/* <div>
+<ul className="video-collection collection">{renderVideoList}</ul>
+</div> */

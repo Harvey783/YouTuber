64bfd40098a79918bffef58ea246e13e15328b07
Chris <chharvey@icloud.com>
2019-03-13 20:32:07 -0400

    SearchBar formatting

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <section className="section section-search red darken-4 white-text center">
+        <div className="container">
+          <div className="row">
+            <div className="col s12">
+              <h4 className="left-align">YouTuber</h4>
+              <div className="input-field">
+                <input
+                  className="white grey-text autocomplete"
+                  placeholder="  Watch Something Now!"
+                  type="text"
+                  id="autocomplete-input"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 

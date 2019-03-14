47d2c0f0247a1f1d308f1798daa705a5a4ba4554
Chris <chharvey@icloud.com>
2019-03-13 21:32:59 -0400

    SearchBar events

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,6 +3,10 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  onInputChange = e => {
+    this.setState({ term: e.target.value });
+  };
+
   render() {
     return (
       <section className="section section-search red darken-4 white-text center">
@@ -10,14 +14,17 @@ class SearchBar extends React.Component {
           <div className="row">
             <div className="col s12">
               <h4 className="left-align">YouTuber</h4>
-              <div className="input-field">
-                <input
-                  className="white grey-text autocomplete"
-                  placeholder="  Watch Something Now!"
-                  type="text"
-                  value={this.state.term}
-                />
-              </div>
+              <form onSubmit={this.onFormSubmit}>
+                <div className="input-field">
+                  <input
+                    className="white grey-text autocomplete"
+                    placeholder="  Watch Something Now!"
+                    type="text"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                  />
+                </div>
+              </form>
             </div>
           </div>
         </div>

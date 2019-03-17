e77ca219d93608eb89089d8edf8bc31704862b9b
Christopher Harvey <chharvey@icloud.com>
2019-03-17 16:51:08 -0400

    test

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
-
-const KEY = 'AIzaSyAfOb0wmpjqxGRC9pSSzlzgP8m_54xy_E8';
+import axios from "axios";
+test;
+const KEY = "AIzaSyAfOb0wmpjqxGRC9pSSzlzgP8m_54xy_E8";
 export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3',
+  baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     maxResults: 5,
     key: KEY,
-    part: 'snippet'
+    part: "snippet"
   }
 });

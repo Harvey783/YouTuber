e8dabc5d448334b4bd25264107888a4a6e20b651
Chris <chharvey@icloud.com>
2019-09-06 22:44:44 -0400

    API fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyAfOb0wmpjqxGRC9pSSzlzgP8m_54xy_E8';
+const KEY = 'AIzaSyD3dTpaUqaFRGTjnoHy63WKXVUbQNY5alY';
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
